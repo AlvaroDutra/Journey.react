ace4081e88ec4838a0091b5f2083919ef90e752e
Álvaro <dutalvaro@gmail.com>
2024-09-11 17:14:48 -0400

    add janela para cadastrar atividade

--- src/pages/trips-details/index.tsx
+++ src/pages/trips-details/index.tsx
@@ -1,6 +1,18 @@
-import { Plus, Calendar, MapPin, Settings2, CircleCheck, Link2, UserCog, CircleDashed } from "lucide-react";
+import { Plus, Calendar, MapPin, Settings2, CircleCheck, Link2, UserCog, CircleDashed, X, Tag, CalendarClock } from "lucide-react";
+import { useState } from "react";
 
 export function TripsDetails(){
+    const [isCreateActivityModalOpen, setIsCreateActivityModalOpen] = useState(false)
+
+    function openCreateActivityModal(){
+        setIsCreateActivityModalOpen(true)
+    }
+
+    function closeCreateActivityModal(){
+        setIsCreateActivityModalOpen(false)
+    }
+    
+
     return (
         <div className="max-w-6xl px-6 py-10 mx-auto space-y-8">
             <div className="px-4 h-16 rounded-xl bg-zinc-900 shadow-shape flex items-center justify-between">
@@ -29,7 +41,7 @@ export function TripsDetails(){
                     <div className="flex items-center justify-between">
                         <h2 className="text-3xl font-semibold">Atividades</h2>
 
-                        <button className="bg-emerald-400 text-emerald-950 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-emerald-500">
+                        <button onClick={openCreateActivityModal} className="bg-emerald-400 text-emerald-950 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-emerald-500">
                             <Plus className='size-5 '/>
                             Cadastrar viagem
                         </button>
@@ -141,6 +153,44 @@ export function TripsDetails(){
                     </div>
                 </div>
             </main>
+
+            {isCreateActivityModalOpen && (
+                <div className='fixed inset-0 bg-black/60 flex items-center justify-center'>
+                <div className='w-[640px] rounded-xl py-5 px-6 shadow-shape bg-zinc-900 space-y-5'>
+                  <div className='space-y-2'>
+                    <div className='flex items-center justify-between'>
+                      <h2 className='text-lg font-semibold'>
+                        Cadastrar atividade
+                      </h2>
+                        <button onClick={closeCreateActivityModal} type='button'>
+                          <X className='size-5 text-zinc-400'/>  
+                        </button>
+                    </div>
+                    <p className='text-sm text-zinc-400'>
+                      Todos os convidados podem visualizar as atividades.
+                    </p>
+                  </div>
+                  
+                    <form className="space-y-3">
+                      <div className='h-14 px-4 bg-zinc-950 border border-zinc-800 rounded-lg flex items-center gap-2'>
+                        <Tag className='text-zinc-400 size-5'/>
+                        <input name='title' placeholder="Qual a atividade ?" className="bg-transparent text-lg placeholder-zinc-400 outline-none flex-1" />
+                      </div>
+                      
+                      <div className="flex items-center gap-2">
+                        <div className='h-14 flex-1 px-4 bg-zinc-950 border border-zinc-800 rounded-lg flex items-center gap-2'>
+                            <CalendarClock className='text-zinc-400 size-5'/>
+                            <input type="datetime-local" name='email' placeholder="Data e hora" className="bg-transparent text-lg placeholder-zinc-400 outline-none flex-1" />
+                        </div>
+                      </div>
+    
+                      <button type='submit' className="bg-emerald-400 w-full justify-center text-emerald-950 rounded-lg px-5 h-11 font-medium flex items-center gap-2 hover:bg-emerald-500">
+                        Salvar atividade
+                      </button>
+                    </form>
+                  </div>
+              </div>
+            )}
 
         </div>
     )
